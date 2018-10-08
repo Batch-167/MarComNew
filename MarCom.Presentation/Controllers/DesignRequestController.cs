1e11273ff89b5d1f16a222e0e658077e67e386bf
Revandra Ardian <ravendra.ardian@gmail.com>
2018-10-08 11:22:43 +0700

    + Controllers/DesignRequestController.cs

---
+++ MarCom.Presentation/Controllers/DesignRequestController.cs
@@ -0,0 +1,26 @@
+﻿using MarCom.Repository;
+using MarCom.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarCom.Presentation.Controllers
+{
+    public class DesignRequestController : Controller
+    {
+        // GET: DesignRequest
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Approve(int id)
+        {
+            ViewBag.Employee = new SelectList(EmployeeRepo.Get(), "Id", "First_Name");
+            DesignApproveViewModel model = DesignApproveRepo.GetById(id);
+            return PartialView("_Approve", model);
+        }
+    }
+}
